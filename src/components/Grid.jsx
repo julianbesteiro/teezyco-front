bf40d43db4e654e6b26dae6bc62a824f51ac65cc
valentin <bellberger45@gmail.com>
2023-05-16 18:44:47 -0300

    responsive grid alpha

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -6,10 +6,10 @@ const Grid = () => {
 
   useEffect(() => {
     let arr = [];
-    for (let i = 0; i <= 29; i++) {
+    for (let i = 0; i <= 31; i++) {
       arr.push({
         id: i,
-        price: 200 + i,
+        price: 2000 + i,
         nombre: "aesthethic 200" + i,
         desc: "remera oversucia aaaa",
         img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/943/997/products/boy-beige1-2e3a2fe4fc6ce264d016676887628942-1024-1024.webp",
@@ -17,44 +17,46 @@ const Grid = () => {
     }
     setProducts(arr);
   }, []);
+  let x = 4;
+  if (window.innerWidth <= 1000) x = 1;
+
   return (
-    <div className="container">
-      {products
-        ? products.map((item, index) => {
-            if (index % 3 === 0) {
-              return (
-                <div className="row" key={index}>
-                  <div className="col">
+    <>
+      {products &&
+        products.length > 0 &&
+        products.map((item, index) => {
+          if (index % x === 0) {
+            const remainingItems = products.slice(index);
+            const itemsToShow =
+              remainingItems.length >= x
+                ? remainingItems.slice(0, x)
+                : remainingItems;
+
+            return (
+              <div className="row" key={index}>
+                {itemsToShow.map((product, subIndex) => (
+                  <div className="col" key={`${index}-${subIndex}`}>
                     <div className="elem">
-                      <img src={item.img} alt="" />
-                      <h3>{item.nombre}</h3>
-                      <h3 className="price">precio: ${item.price}</h3>
-                    </div>
-                  </div>
-                  <div className="col">
-                    <div className="elem">
-                      <img src={products[index + 1].img} alt="" />
-                      <h3>{products[index + 1].nombre}</h3>
+                      <img src={product.img} alt="" />
+                      <div className=" icons">
+                        <p className="favs">♡</p>
+                        <p className="carrito">🛒</p>
+                      </div>
+                      <h3>{product.nombre}</h3>
                       <h3 className="price">
-                        precio: ${products[index + 1].price}
+                        precio: ${product.price} o 6 cuotas de $
+                        {Math.floor(product.price / 5)}
                       </h3>
                     </div>
                   </div>
-                  <div className="col">
-                    <div className="elem">
-                      <img src={products[index + 1].img} alt="" />
-                      <h3>{products[index + 2].nombre}</h3>
-                      <h3 className="price">
-                        precio: ${products[index + 2].price}
-                      </h3>
-                    </div>
-                  </div>
-                </div>
-              );
-            }
-          })
-        : null}
-    </div>
+                ))}
+              </div>
+            );
+          } else {
+            return null;
+          }
+        })}
+    </>
   );
 };
 
